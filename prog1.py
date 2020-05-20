d9ba4c3d2b48620ba74009d9b179b29c7e6c96e6
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-05-20 13:25:35 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,14 @@
+>>> import numpy
+>>> import matplotlib
+>>> import pandas
+>>> import pandas as pd
+>>> url="https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv"
+>>> df=pd.read_csv(url,delimiter=',')
+>>> print(df.head(5))
+   sepal_length  sepal_width  petal_length  petal_width species
+0           5.1          3.5           1.4          0.2  setosa
+1           4.9          3.0           1.4          0.2  setosa
+2           4.7          3.2           1.3          0.2  setosa
+3           4.6          3.1           1.5          0.2  setosa
+4           5.0          3.6           1.4          0.2  setosa
+>>> 
