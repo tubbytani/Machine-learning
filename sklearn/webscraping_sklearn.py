8bc4dae6a376a424f758809f92c9c30f6c9a27c7
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-05-25 10:36:47 +0530

    Create webscraping_sklearn.py

---
+++ sklearn/webscraping_sklearn.py
@@ -0,0 +1,12 @@
+##bye=[]
+##def bye("welcome"):         
+##    for j in wordnet.synsets(word):
+##        for l in j.lemmas():
+##            bye.append(l.name())
+##print("Are you satisfied with the reviews? ")
+##flag=True
+##while(flag=True):
+##    user_response = input()
+##    user_response=user_response.lower()
+##    if(user_response!='yes'):
+##        if
